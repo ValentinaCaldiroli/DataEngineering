2ff74f8bcace9d8a1946e41c5308939ff0921ead
Valentina Caldiroli <valecaldirolibalenzuela@gmail.com>
2024-03-03 12:14:06 -0300

    Creación de la tabla destino

--- Entregable1 - Valentina Caldiroli.py
+++ Entregable1 - Valentina Caldiroli.py
@@ -1,18 +1,16 @@
 from newsapi import NewsApiClient
 import psycopg2
+from datetime import datetime
 
 newsapi = NewsApiClient(api_key='d0b09d44ab00472b8b2b4ecb8b016e13')
 
 # Obtener las principales noticias en español
-top_headlines_es = newsapi.get_top_headlines()
+top_headlines = newsapi.get_top_headlines()
 
 
 # Imprimir los títulos de las noticias
-for article in top_headlines_es['articles']:
+for article in top_headlines['articles']:
     print(article['title'])
-
-
-
 
 # Conexión a Redshift
     
@@ -28,8 +26,29 @@ try:
         password = pwd,
         port='5439'
     )
-    print("Conectado a Redshift con éxito!")
+    print("Conectado correctamente")
     
 except Exception as e:
-    print("No es posible conectar a Redshift")
+    print("No es posible la conexión")
     print(e)
+
+# Creación de la tabla
+
+with conn.cursor() as cur:
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS coder_noticias
+        (
+            id VARCHAR(50) primary key,
+            Fuente VARCHAR(100),
+            Titulo VARCHAR(300),
+            Autor VARCHAR(255),
+            URL VARCHAR(300),
+            Fecha_publicacion date,
+            Fecha_carga date
+        )
+    """)
+    conn.commit()
+
+    cur.execute("TRUNCATE TABLE coder_noticias")
+    count = cur.rowcount
+
