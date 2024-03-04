fbbc9026aa38194126d927191a5f6f83564e17bb
Valentina Caldiroli <valecaldirolibalenzuela@gmail.com>
2024-03-04 19:27:08 -0300

    Final

--- Entregable1 - Valentina Caldiroli.py
+++ Entregable1 - Valentina Caldiroli.py
@@ -1,16 +1,9 @@
-from newsapi import NewsApiClient
+from newsapi import NewsApiClient as Nw
 import psycopg2
 from datetime import datetime
+import pandas as pd 
 
-newsapi = NewsApiClient(api_key='d0b09d44ab00472b8b2b4ecb8b016e13')
-
-# Obtener las principales noticias en español
-top_headlines = newsapi.get_top_headlines()
-
-
-# Imprimir los títulos de las noticias
-for article in top_headlines['articles']:
-    print(article['title'])
+newsapi = Nw(api_key='d0b09d44ab00472b8b2b4ecb8b016e13')
 
 # Conexión a Redshift
     
@@ -38,13 +31,15 @@ with conn.cursor() as cur:
     cur.execute("""
         CREATE TABLE IF NOT EXISTS coder_noticias
         (
-            id VARCHAR(50) primary key,
-            Fuente VARCHAR(100),
+            
             Titulo VARCHAR(300),
+            Fuente_id VARCHAR(50),
+            Fuente VARCHAR(100),
             Autor VARCHAR(255),
             URL VARCHAR(300),
             Fecha_publicacion date,
-            Fecha_carga date
+            Fecha_carga date,
+            primary key(Titulo, Fuente)
         )
     """)
     conn.commit()
@@ -52,3 +47,24 @@ with conn.cursor() as cur:
     cur.execute("TRUNCATE TABLE coder_noticias")
     count = cur.rowcount
 
+# Trabajo con los datos 
+    
+resultado = newsapi.get_everything(q='Argentina',
+                       sort_by='publishedAt',
+                        language='es',
+                        page_size=100)
+datos = {'Titulo': [], 'Fuente_id': [],'Fuente': [], 'Autor': [], 'URL': [],'Fecha_publicacion': []}
+for noticia in resultado['articles']:
+
+    datos['Titulo'].append(noticia['title'])
+    datos['Fuente_id'].append(noticia['source']['id'])
+    datos['Fuente'].append(noticia['source']['name'])
+    datos['Autor'].append(noticia['author'])
+    datos['URL'].append(noticia['url'])
+    datos['Fecha_publicacion'].append(noticia['publishedAt'])
+
+df = pd.DataFrame(datos)
+df.drop_duplicates(subset=['Fuente', 'Autor'], keep='first', inplace=True)
+df['Fuente_id'].fillna('Sin datos', inplace=True)
+df.loc[df['Fuente_id'] == '', 'Fuente_id'] = 'Sin datos'
+print(df)
